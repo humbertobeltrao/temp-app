59313148cc0de5e4e1bb81b8ee540e58f1ba8d95
Humberto <humbertobeltrao@gmail.com>
2022-04-07 20:17:48 -0300

    test

--- test/new_test.dart
+++ test/new_test.dart
@@ -2,7 +2,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:temp_app/functions.dart';
 
 void main() {
-  group('Conversor de Temperatura', () {
+  group('Conversor de Temperatu', () {
     test('teste de celsius para fahrenheit', () {
       expect(Functions.paraFahrenheit(0), 32);
     });
